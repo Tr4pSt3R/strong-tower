44fc62cc277138b5ea01e063cd9232471a07bb12
Jones Agyemang <jones.k.agyemang@gmail.com>
2013-09-09 12:46:49 +0000

    Add a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Prayer.new(:title => "The Lord's Prayer", :content => "Our Father in heaven, hallowed be thy name, your kingdom come, your will be done on earth as it is in heaven. Give us today our daily bread. Forgive us our debts, as we have forgiven our debtors, and lead us not into temptations, but deliver us from the evil one. Amen!", :credit_to => "Book of Matthew", :remarks => "The disciples asked Jesus how to pray")
